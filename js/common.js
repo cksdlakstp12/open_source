747502657c4f8b7872ab89ab7d22d6679c4a2b2b
이찬 <cksdlakstp12@khu.ac.kr>
2020-06-05 22:14:44 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -30,12 +30,13 @@ function joonsikfinder (value) {
   var bodyEle = document.getElementsByTagName('body')[0]
   var titleIdentifiers = document.getElementsByClassName('title-identifier')
   if (value === '엄준식') {
-    bodyEle.className = ''	
-    //bodyEle.classList.add('joonsik')	
+    bodyEle.className = ''
     titleIdentifiers[0].innerHTML = '엄준식'	
     titleIdentifiers[1].innerHTML = '엄준식을'
-  } else if (value === '박원순') {
-  } else if (value === '트럼프') {
+  } else if (value === '김응삼') {
+    bodyEle.className = ''	
+    titleIdentifiers[0].innerHTML = '김응삼'	
+    titleIdentifiers[1].innerHTML = '김응삼을'
   } else {
     bodyEle.className = ''
     titleIdentifiers[0].innerHTML = '악질 이름'
