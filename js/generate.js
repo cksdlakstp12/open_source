e825c8f46177e001dec4947d07a6f89353ebe077
이찬 <cksdlakstp12@khu.ac.kr>
2020-06-11 18:14:00 +0900

    조사가 필요한 특별한 경우 정의 수정

--- js/generate.js
+++ js/generate.js
@@ -34,10 +34,7 @@ function generateResult(varName) {
 
 
     // 조사가 필요한 특별한 경우 정의
-    if(randomDetailBehavior === '만흥분하는'){
-      var randomObject = objects[Math.floor(Math.random() * objects.length)]
-      return randomDetailLocation + josa.r(randomObject, '으로/로') + randomDetailBehavior + randomDetailJob + varName
-    } else if(randomDetailBehavior === '선물받고좋아하는'){
+    if(randomDetailBehavior === '선물받고좋아하는'){
       var randomObject = objects[Math.floor(Math.random() * objects.length)]
       return randomDetailLocation + josa.r(randomObject, '을/를') + randomDetailBehavior + randomDetailJob + varName
     }
